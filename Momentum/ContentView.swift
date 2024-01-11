5b6ad624ab96f179489c9318f9ce749fdaa6f631
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-01-11 13:05:25 -0800

    Initial Commit

---
+++ Momentum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Momentum
+//
+//  Created by Marcus Ziadé on 11.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Marcus Ziadé on 11.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
